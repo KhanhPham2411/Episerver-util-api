23903abafab2c4f3e16c5f843859b49ca67b2559
KhanhPham2411 <phamduckhanh2411@gmail.com>
2022-06-08 10:38:30 +0700

    init

---
+++ catalog-group/MetafieldApiController.cs
@@ -0,0 +1,60 @@
+﻿using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.Reference.Commerce.Shared.Identity;
+using Mediachase.BusinessFoundation.Data;
+using Mediachase.BusinessFoundation.Data.Meta.Management;
+using Mediachase.Commerce.Customers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foundation.Custom
+{
+    [ApiController]
+    [Route("metafield-api")]
+    public class MetafieldApiController : ControllerBase
+    {
+        public MetafieldApiController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("AddMetaFieldCheckboxBoolean")]
+        public async Task<ActionResult<string>> Test([FromQuery] string firstName = null)
+        {
+            string name = "Attends";
+            string friendlyName = name;
+
+            var typeName = MetaFieldType.CheckboxBoolean;
+            var orgMetaClass = DataContext.Current.MetaModel.MetaClasses[OrganizationEntity.ClassName];
+            var metaClass = orgMetaClass;
+
+            string log = "";
+            var existingField = metaClass.Fields[name];
+            if (existingField == null)
+            {
+                var attributes = new Mediachase.BusinessFoundation.Data.Meta.Management.AttributeCollection
+            {
+                { McDataTypeAttribute.BooleanLabel, friendlyName },
+                { McDataTypeAttribute.EnumEditable, true }
+            };
+                metaClass.CreateMetaField(name, friendlyName, typeName, attributes); // DeleteMetaField
+                using (var myEditScope = DataContext.Current.MetaModel.BeginEdit())
+                {
+                    metaClass.Fields[name].AccessLevel = AccessLevel.Development;
+                    metaClass.Fields[name].Owner = "Development";
+                    myEditScope.SaveChanges();
+                }
+                log += String.Format("Meta field {0} is added to meta class {1}", name, OrganizationEntity.ClassName);
+            }
+            else
+            {
+                log += String.Format("Meta field {0} is already exist in meta class {1}", name, OrganizationEntity.ClassName);
+            }
+
+            return Ok(log);
+        }
+    }
+}
---
+++ user-group/UserApiController.cs
@@ -0,0 +1,36 @@
+﻿using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.Reference.Commerce.Shared.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foundation.Custom
+{
+    [ApiController]
+    [Route("user-api")]
+    public class UserApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext<SiteUser> _db;
+        public UserApiController(ApplicationDbContext<SiteUser> db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        [Route("test-user")]
+        public async Task<ActionResult<string>> TestUser([FromQuery] string firstName = null)
+        {
+            var user = HttpContext.User.Identity;
+            var userDb = await _db.Users.FirstOrDefaultAsync(u => u.Email.ToLower().Equals(user.Name.ToLower()));
+            if (!string.IsNullOrEmpty(firstName))
+            {
+                userDb.FirstName = firstName;
+                _db.Users.Update(userDb);
+                await _db.SaveChangesAsync();
+                userDb = await _db.Users.FirstOrDefaultAsync(u => u.Email.ToLower().Equals(user.Name.ToLower()));
+            }
+            return Ok(userDb.FirstName ?? "it's null");
+        } 
+    }
+}
