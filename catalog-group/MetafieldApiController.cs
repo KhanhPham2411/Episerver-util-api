101ce13ab5a5335afc88f792b92aa03b7b21ed1d
KhanhPham2411 <phamduckhanh2411@gmail.com>
2022-06-08 10:49:39 +0700

    AddMetaFieldCheckboxBooleanAddress

--- catalog-group/MetafieldApiController.cs
+++ catalog-group/MetafieldApiController.cs
@@ -21,8 +21,8 @@ namespace Foundation.Custom
         }
 
         [HttpGet]
-        [Route("AddMetaFieldCheckboxBoolean")]
-        public async Task<ActionResult<string>> Test([FromQuery] string firstName = null)
+        [Route("AddMetaFieldCheckboxBooleanOrganization")]
+        public async Task<ActionResult<string>> AddMetaFieldCheckboxBooleanOrganization([FromQuery] string firstName = null)
         {
             string name = "Attends";
             string friendlyName = name;
@@ -52,6 +52,43 @@ namespace Foundation.Custom
             else
             {
                 log += String.Format("Meta field {0} is already exist in meta class {1}", name, OrganizationEntity.ClassName);
+            }
+
+            return Ok(log);
+        }
+        
+        [HttpGet]
+        [Route("AddMetaFieldCheckboxBooleanAddress")]
+        public async Task<ActionResult<string>> AddMetaFieldCheckboxBooleanAddress([FromQuery] string firstName = null)
+        {
+            string name = "Attends";
+            string friendlyName = name;
+
+            var typeName = MetaFieldType.CheckboxBoolean;
+            var metaClassName = AddressEntity.ClassName;
+            var metaClass = DataContext.Current.MetaModel.MetaClasses[metaClassName];
+
+            string log = "";
+            var existingField = metaClass.Fields[name];
+            if (existingField == null)
+            {
+                var attributes = new Mediachase.BusinessFoundation.Data.Meta.Management.AttributeCollection
+            {
+                { McDataTypeAttribute.BooleanLabel, friendlyName },
+                { McDataTypeAttribute.EnumEditable, true }
+            };
+                metaClass.CreateMetaField(name, friendlyName, typeName, attributes); // DeleteMetaField
+                using (var myEditScope = DataContext.Current.MetaModel.BeginEdit())
+                {
+                    metaClass.Fields[name].AccessLevel = AccessLevel.Development;
+                    metaClass.Fields[name].Owner = "Development";
+                    myEditScope.SaveChanges();
+                }
+                log += String.Format("Meta field {0} is added to meta class {1}", name, metaClassName);
+            }
+            else
+            {
+                log += String.Format("Meta field {0} is already exist in meta class {1}", name, metaClassName);
             }
 
             return Ok(log);
