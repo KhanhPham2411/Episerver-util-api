03288593b47c90d84bab47fc4cc375dd36cb1326
KhanhPham2411 <phamduckhanh2411@gmail.com>
2022-06-28 16:28:19 +0700

    MediaApiController

---
+++ catalog-group/MediaApiController.cs
@@ -0,0 +1,69 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.Blobs;
+using EPiServer.ServiceLocation;
+using Foundation.Features.CatalogContent.Product;
+using Mediachase.Commerce.Catalog;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+
+namespace Foundation.Custom
+{
+    [ApiController]
+    [Route("media-api")]
+    public class MediaApiController : ControllerBase
+    {
+        private readonly IContentLoader _contentLoader;
+
+
+        public MediaApiController(IContentLoader contentLoader)
+        {
+            _contentLoader = contentLoader;
+        }
+
+        private FileContentResult DownloadInternal(ContentReference contentRef)
+        {
+            var mediaData = _contentLoader.Get<IContent>(contentRef) as MediaData;
+            if (mediaData != null)
+            {
+                var downloadFile = mediaData;
+                if (downloadFile != null)
+                {
+                    var blob = downloadFile.BinaryData as FileBlob;
+                    if (blob != null)
+                    {
+                        var routeSegment = downloadFile.RouteSegment;
+                        var extension = Path.GetExtension(blob.FilePath) ?? "";
+                        var downloadFileName = routeSegment.EndsWith(extension) ? routeSegment : routeSegment + extension;
+
+                        HttpContext.Response.Headers.Add("content-disposition", "attachment;filename=" + Path.GetFileName(downloadFileName));
+                        return File(System.IO.File.ReadAllBytes(blob.FilePath), "application/octet-stream");
+                    }
+                }
+            }
+            return null;
+        }
+
+
+        [HttpGet("DownloadProductImage/{contentLinkId}")]
+        public IActionResult DownloadProductImage(string productCode)
+        {
+            var _referenceConverter = ServiceLocator.Current.GetInstance<ReferenceConverter>();
+            var _contentRepository = ServiceLocator.Current.GetInstance<IContentRepository>();
+
+            var productContentLink = _referenceConverter.GetContentLink(productCode ?? "p-39813617");
+            var product = _contentRepository.Get<GenericProduct>(productContentLink).CreateWritableClone<GenericProduct>();
+
+            var firstImage = product.CommerceMediaCollection.First().AssetLink;
+            return DownloadInternal(firstImage);
+        }
+
+
+        [HttpGet("Download/{contentLinkId}")]
+        public IActionResult Download(int contentLinkId)
+        {
+            return DownloadInternal(new ContentReference(contentLinkId));
+        }
+    }
+}
--- catalog-group/ProductApiController.cs
+++ catalog-group/ProductApiController.cs
@@ -1,14 +1,16 @@
 ﻿using EPiServer;
 using EPiServer.ServiceLocation;
+using EPiServer.Shell;
 using Foundation.Features.CatalogContent.Product;
 using Mediachase.Commerce.Catalog;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Foundation.Custom
 {
     [ApiController]
-    [Route("product")]
+    [Route("product-api")]
     public class ProductApiController : ControllerBase
     {
         public ProductApiController()
@@ -33,5 +35,25 @@ namespace Foundation.Custom
             return Ok(log);
         }
 
+        [HttpGet]
+        [Route("GetProductWithMedia")]
+        public async Task<ActionResult<string>> GetProductWithMedia([FromQuery] string code = null)
+        {
+            var _referenceConverter = ServiceLocator.Current.GetInstance<ReferenceConverter>();
+            var _contentRepository = ServiceLocator.Current.GetInstance<IContentRepository>();
+            string log = "";
+
+            var productContentLink = _referenceConverter.GetContentLink(code ?? "p-39813617");
+            var product = _contentRepository.Get<GenericProduct>(productContentLink).CreateWritableClone<GenericProduct>();
+
+            var images = product.CommerceMediaCollection?.Select(x => x.AssetLink.GetUri()).ToList();
+
+            
+            log += product.Code + "\n";
+            log += product.ContentGuid.ToString() + "\n";
+            log += string.Join(",", images.Select(w => w.ToString()));
+
+            return Ok(log);
+        }
     }
 }
