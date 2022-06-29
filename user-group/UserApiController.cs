ef72470dc201ba69dcb9e93bc6f428149e3a0a7d
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2022-06-29 13:33:34 +0700

    Update UserApiController.cs

--- user-group/UserApiController.cs
+++ user-group/UserApiController.cs
@@ -31,5 +31,55 @@ namespace Foundation.Custom
             }
             return Ok(userDb.FirstName ?? "it's null");
         } 
+        
+        [HttpGet]
+        [Route("create-user")]
+        public async Task<ActionResult<string>> CreateUser([FromQuery] string username = null)
+        {
+            var uiSignInManager = ServiceLocator.Current.GetInstance<UISignInManager>();
+            var uiUserProvider = ServiceLocator.Current.GetInstance<UIUserProvider>();
+            var uiRoleProvider = ServiceLocator.Current.GetInstance<UIRoleProvider>();
+            var uiUserManager = ServiceLocator.Current.GetInstance<UIUserManager>();
+
+            username = username ?? "sysadmin123";
+            var email = username + "@episerver.com";
+            var password = "P@ssw0rd";
+            var log = "";
+            string[] roles = { "Administrators" };
+
+            var userList = uiUserProvider.FindUsersByNameAsync(username, 0, 1);
+            var user = await userList.FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+
+                var result = await uiUserProvider.CreateUserAsync(username, password, email, null, null, true);
+                foreach (var error in result.Errors)
+                {
+                    log += error;
+                }
+                if (result.Errors.Count() == 0)
+                {
+                    string role = roles.First();
+                    var isExist = await uiRoleProvider.RoleExistsAsync(role);
+                    if (!isExist)
+                    {
+                        await uiRoleProvider.CreateRoleAsync(role);
+                    }
+                    await uiRoleProvider.AddUserToRolesAsync(username, roles);
+                    log += "Created successfully user: " + username;
+                }
+            }
+            else
+            {
+                log += ("Failed to create as the user " + username + " already exist");
+                return Ok(log);
+            }
+            log += "User name: " + username;
+            log += "Password: " + password;
+            log += "Email: " + email;
+
+            return Ok(log);
+        } 
     }
 }
