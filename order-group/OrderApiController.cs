4643e706f1202fc0aa625d344e09df6a34691cce
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2024-02-19 15:26:10 +0700

    Update OrderApiController.cs

--- order-group/OrderApiController.cs
+++ order-group/OrderApiController.cs
@@ -1,7 +1,10 @@
-﻿using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.Cms.UI.AspNetIdentity;
 using EPiServer.Commerce.Marketing;
+using EPiServer.Commerce.Order;
 using EPiServer.Core;
 using EPiServer.ServiceLocation;
+using Foundation.Social.Models;
+using Geta.NotFoundHandler.Admin.Pages.Geta.NotFoundHandler.Admin.Models;
 using Mediachase.BusinessFoundation.Data;
 using Mediachase.BusinessFoundation.Data.Meta.Management;
 using Mediachase.Commerce;
@@ -11,6 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 
 namespace Foundation.Custom
@@ -25,12 +29,36 @@ namespace Foundation.Custom
         }
 
         [HttpGet]
-        [Route("search")]
-        public async Task<ActionResult<string>> Search([FromQuery] string id = null)
+        [Route("FindPurchaseOrdersByStatus")]
+        public async Task<ActionResult<string>> FindPurchaseOrdersByStatus([FromQuery] string id = null)
         {
             var orders = OrderContext.Current.FindPurchaseOrdersByStatus(OrderStatus.InProgress);
 
             return Content(string.Join("|", orders.Select(t => t.Name)));
+        }
+
+        [HttpGet]
+        [Route("FindPurchaseOrders")]
+        public async Task<ActionResult<string>> FindPurchaseOrders([FromQuery] string id = null)
+        {
+            var _orderSearchService = ServiceLocator.Current.GetInstance<IOrderSearchService>();
+
+            OrderSearchFilter filter = new()
+            {
+                ReturnTotalCount = true,
+                CustomerId = new Guid("CD0DEE6E-6F76-4B29-A5E3-AE095DE807B6"),
+                //StartingIndex = paging.StartingIndex(),
+                //RecordsToRetrieve = paging.Take(),
+                //SiteId = site.Id.ToString(),
+            };
+
+            var results = _orderSearchService.FindPurchaseOrders(filter);
+            var orders = results.Orders.ToList();
+            if (orders.Count == 0) {
+                return Content("No order found");
+            }
+
+            return Content(string.Join("|", orders.Select(t => t.OrderNumber)));
         }
     }
 }
