f9161b47c6d92c389aea4d314857523a1d697bce
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2024-08-20 11:08:42 +0700

    Update content-type.sql

--- sql_scripts/content-type.sql
+++ sql_scripts/content-type.sql
@@ -1,9 +1,11 @@
-
-SELECT TOP (1000) c.[pkID],
-  ct.Name
-      ,[fkContentTypeID]
-      ,[fkParentID]
+SELECT TOP (1000) c.[pkID]
+	,ct.[Name]
+	,cl.[URLSegment]
+	,[fkParentID]
+    ,[fkContentTypeID]
+      
 
   FROM [dbo].[tblContent] c
   JOIN [dbo].[tblContentType] ct on c.fkContentTypeID = ct.pkID
-  where c.pkID in (326, 173, 3, 1)
+  JOIN [dbo].[tblContentLanguage] cl on cl.fkContentID = c.pkID
+  where c.pkID in (278952, 112060, 111111, 3, 1)
