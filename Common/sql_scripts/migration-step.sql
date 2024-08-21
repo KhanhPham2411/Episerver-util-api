9e8dd622bc654db505eb3dbcd9f9b20618ad14a8
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2024-08-21 14:10:27 +0700

    Create migration-step.sql

---
+++ Common/sql_scripts/migration-step.sql
@@ -0,0 +1,4 @@
+-- Compare the result between environment to check if all migration steps are run successfully
+SELECT TOP (1000) String01, StoreName, Boolean01, DateTime01
+FROM [dbo].[tblBigTable]
+WHere StoreName like '%Step%'
