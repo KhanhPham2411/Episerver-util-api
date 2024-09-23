7a970c60962a8cb8af4697bc5950172f1364978c
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2024-09-23 14:09:07 +0700

    Update content-info.sql

--- Common/sql_scripts/content-info.sql
+++ Common/sql_scripts/content-info.sql
@@ -1,11 +1,11 @@
 SELECT TOP (1000) c.[pkID]
 	,ct.[Name]
 	,cl.[URLSegment]
+	,cl.[Created]
 	,[fkParentID]
-    ,[fkContentTypeID]
-      
+  ,[fkContentTypeID]
 
   FROM [dbo].[tblContent] c
   JOIN [dbo].[tblContentType] ct on c.fkContentTypeID = ct.pkID
   JOIN [dbo].[tblContentLanguage] cl on cl.fkContentID = c.pkID
-  where c.pkID in (278952, 112060, 111111, 3, 1)
+  where c.pkID in (87665, 88181)
