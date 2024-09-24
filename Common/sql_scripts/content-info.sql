a67a32e7e34b15080e33f8f3db3c2cadb4a95b9f
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2024-09-24 16:31:22 +0700

    Update content-info.sql

--- Common/sql_scripts/content-info.sql
+++ Common/sql_scripts/content-info.sql
@@ -10,3 +10,17 @@ SELECT TOP (1000) c.[pkID]
   JOIN [dbo].[tblContentType] ct on c.fkContentTypeID = ct.pkID
   JOIN [dbo].[tblContentLanguage] cl on cl.fkContentID = c.pkID
   where c.pkID in (87665, 88181)
+
+-- Add mapped identity info
+SELECT TOP (1000) c.[pkID]
+	,cl.[Created],cl.[Name] ,ct.[Name] as TypeName
+	,mi.[Provider] ,mi.[ProviderUniqueID]
+	,cl.[URLSegment]
+	,[fkParentID]
+  	,[fkContentTypeID]
+FROM [dbo].[tblContent] c
+  JOIN [dbo].[tblContentType] ct on c.fkContentTypeID = ct.pkID
+  JOIN [dbo].[tblContentLanguage] cl on cl.fkContentID = c.pkID
+  LEFT JOIN [dbo].[tblMappedIdentity] mi on mi.[ExistingContentId] = c.pkID
+WHERE c.pkID in (88387, 18971)
+
