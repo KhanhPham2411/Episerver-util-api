ed97e3d903e502c61d972fadbefb26e0e77caed8
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2025-01-08 16:39:44 +0700

    Update scheduled-job.sql

--- Common/sql_scripts/scheduled-job.sql
+++ Common/sql_scripts/scheduled-job.sql
@@ -1,10 +1,13 @@
-SELECT  TOP(10) item.Name, log.[Text],
-	(log.Duration) * POWER(10.00000000000,-7) / 60 as Minutes,
-	log.[Exec], log.Duration, 
+SELECT  TOP(100) item.Name, log.[Text],
+	log.[Exec], 
+	-- log.Duration, 
+	-- (log.Duration) * POWER(10.00000000000,-7) / 60 as Minutes,
     (log.Duration) * POWER(10.00000000000,-7) / 360 as Seconds,
-        item.DatePart, item.Interval, item.LastExec, item.NextExec, log.[Server]
+    item.DatePart, item.Interval, 
+	log.[Server],
+	item.LastExec, item.NextExec 
 FROM [tblScheduledItemLog] as log INNER JOIN [tblScheduledItem] as item
 ON log.fkScheduledItemId = item.pkID
 --where CONVERT(VARCHAR(25), log.[Exec], 126) LIKE '2020-06-29%' AND log.Text like  '%publish%'
-where item.Name LIKE '%Trim%'
+where item.Name LIKE '%Order Export%' AND CONVERT(VARCHAR(25), log.[Exec], 126) LIKE '2025-01-06%'
 order by log.[Exec] desc
