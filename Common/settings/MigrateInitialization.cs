7dc8bf9397fd4774754a57eecb2e5d47782e1de4
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2025-03-10 15:49:22 +0700

    Update MigrateInitialization.cs

--- Common/settings/MigrateInitialization.cs
+++ Common/settings/MigrateInitialization.cs
@@ -1,29 +1,30 @@
-using EPiServer.ServiceLocation;
 using EPiServer.Framework.Initialization;
-using EPiServer.Security;
 using EPiServer.Framework;
 using EPiServer.Commerce.Internal.Migration;
+using EPiServer.ServiceLocation;
 
 namespace Foundation.Custom
 {
-	[ModuleDependency(typeof(EPiServer.Shell.UI.InitializationModule))]
+    [ModuleDependency(typeof(EPiServer.Shell.UI.InitializationModule))]
     [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
     [ModuleDependency(typeof(ServiceContainerInitialization))]
     public class MigrateInitialization : IConfigurableModule
-	{
-		public void ConfigureContainer(ServiceConfigurationContext context)
-		{
-			
-		}
-        public void Initialize(InitializationEngine context)
-        { 
-            var manager = ServiceLocator.Current.GetInstance<MigrationManager>();
-            manager.Migrate();
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
         }
 
-		public void Uninitialize(InitializationEngine context)
-		{
-			
-		}
-	}
+        public void Initialize(InitializationEngine context)
+        {
+            var manager = context.Locate.Advanced.GetInstance<MigrationManager>();
+            if (manager.SiteNeedsToBeMigrated())
+            {
+                manager.Migrate();
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
 }
