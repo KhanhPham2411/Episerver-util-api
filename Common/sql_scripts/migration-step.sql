f9aa99225956710e09a741b907fd7c17a71b429b
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2025-03-10 15:36:00 +0700

    Update migration-step.sql

--- Common/sql_scripts/migration-step.sql
+++ Common/sql_scripts/migration-step.sql
@@ -1,5 +1,14 @@
--- Compare the result between environment to check if all migration steps are run successfully
+-- Compare the result between the environment to check if all migration steps are run successfully
 SELECT TOP (1000) String01, StoreName, Boolean01, DateTime01
 FROM [dbo].[tblBigTable]
 WHere StoreName like '%Step%'
 ORDER BY DateTime01 desc
+
+
+-- Testing to trigger the migration
+UPDATE [dbo].[tblBigTable]
+   SET Boolean01 = 0
+ WHERE String01 = 'EPiServer.Commerce.Internal.Migration.Steps.RemoveOrphanedMetaKeysStep'
+GO
+
+
