7f543c277a5087b00b9b74ad8b5db80e37f2f354
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2025-03-12 10:53:14 +0700

    Update migration-step.sql

--- Common/sql_scripts/migration-step.sql
+++ Common/sql_scripts/migration-step.sql
@@ -1,8 +1,8 @@
 -- Compare the result between the environment to check if all migration steps are run successfully
 SELECT TOP (1000) String01, StoreName, Boolean01, DateTime01
 FROM [dbo].[tblBigTable]
-WHere StoreName like '%Step%'
-ORDER BY DateTime01 desc
+WHERE StoreName LIKE '%Step%'
+ORDER BY DateTime01 DESC
 
 
 -- Testing to trigger the migration
