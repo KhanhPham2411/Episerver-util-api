fc4b8c4c0b2eae28a95320e89cb662442766d691
Duc Khanh Pham <phamduckhanh2411@gmail.com>
2025-05-12 16:30:52 +0700

    Update content-info.sql

--- Common/sql_scripts/content-info.sql
+++ Common/sql_scripts/content-info.sql
@@ -1,26 +1,12 @@
-SELECT TOP (1000) c.[pkID]
-	,cl.[Name]
-	,cl.[URLSegment]
-	,ct.[Name] as TypeName
-	,cl.[Created]
-	,[fkParentID]
-  ,[fkContentTypeID]
-
-  FROM [dbo].[tblContent] c
-  JOIN [dbo].[tblContentType] ct on c.fkContentTypeID = ct.pkID
-  JOIN [dbo].[tblContentLanguage] cl on cl.fkContentID = c.pkID
-  where c.pkID in (87665, 88181)
-
--- Add mapped identity info
-SELECT TOP (1000) c.[pkID]
+SELECT TOP (1000) c.[pkID], lb.[LanguageID]
 	,cl.[Created],cl.[Name] ,ct.[Name] as TypeName
-	,mi.[Provider] ,mi.[ProviderUniqueID]
 	,cl.[URLSegment]
 	,[fkParentID]
   	,[fkContentTypeID]
+	,mi.[Provider] ,mi.[ProviderUniqueID]
 FROM [dbo].[tblContent] c
   JOIN [dbo].[tblContentType] ct on c.fkContentTypeID = ct.pkID
   JOIN [dbo].[tblContentLanguage] cl on cl.fkContentID = c.pkID
+  JOIN [dbo].[tblLanguageBranch] lb on lb.pkID =  cl.[fkLanguageBranchID]
   LEFT JOIN [dbo].[tblMappedIdentity] mi on mi.[ExistingContentId] = c.pkID
-WHERE c.pkID in (88387, 18971)
-
+WHERE cl.[URLSegment] like '%test%'
