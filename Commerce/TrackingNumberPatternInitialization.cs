21a5908eb9ec512ac02415ee6951469a13d2cc56
Duc Khanh Pham <DucKhanh.Pham@episerver.com>
2025-10-14 15:37:45 +0700

    Add TrackingNumberPatternInitialization module to set tracking number pattern for orders

---
+++ Commerce/TrackingNumberPatternInitialization.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Framework.Initialization;
+using EPiServer.Framework;
+using Mediachase.Commerce.Orders;
+
+namespace Foundation.Custom.Episerver_util_api.Commerce
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
+    public class TrackingNumberPatternInitialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            OrderContext.Current.TrackingNumberPattern = "^[A-Za-z0-9-]+$";
+        }
+
+        public void Uninitialize(InitializationEngine context) { }
+    }
+}
